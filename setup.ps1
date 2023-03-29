7c93a94fbea9da40ea850fe1eddf08f3660818d4
dennishuyen <chandat@hotmail.de>
2023-03-29 22:32:01 +0200

    Create setup.ps1

---
+++ setup.ps1
@@ -0,0 +1,36 @@
+# Handle cases where the user has multiple subscriptions
+$subs = Get-AzSubscription | Select-Object
+if($subs.GetType().IsArray -and $subs.length -gt 1){
+    Write-Host "You have multiple Azure subscriptions - please select the one you want to use:"
+    for($i = 0; $i -lt $subs.length; $i++)
+    {
+            Write-Host "[$($i)]: $($subs[$i].Name) (ID = $($subs[$i].Id))"
+    }
+    $selectedIndex = -1
+    $selectedValidIndex = 0
+    while ($selectedValidIndex -ne 1)
+    {
+            $enteredValue = Read-Host("Enter 0 to $($subs.Length - 1)")
+            if (-not ([string]::IsNullOrEmpty($enteredValue)))
+            {
+                if ([int]$enteredValue -in (0..$($subs.Length - 1)))
+                {
+                    $selectedIndex = [int]$enteredValue
+                    $selectedValidIndex = 1
+                }
+                else
+                {
+                    Write-Output "Please enter a valid subscription number."
+                }
+            }
+            else
+            {
+                Write-Output "Please enter a valid subscription number."
+            }
+    }
+    $selectedSub = $subs[$selectedIndex].Id
+    Select-AzSubscription -SubscriptionId $selectedSub
+    az account set --subscription $selectedSub
+}
+
+Set-AzSynapseLinkedService -WorkspaceName synapsez364ima -Name HttpServer2 -DefinitionFile "./linkedService/HttpServer2.json"
